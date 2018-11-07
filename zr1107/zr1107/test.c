b7b3d96472338fb42c2f1e56540ee9df76b8d1ae
zr1997 <1007469685@qq.com>
2018-11-07 23:54:00 +0800

    创建一个数组 实现函数init（）初始化数组； 实现empty（）清空数组； 实现reverse（）函数完成数组元素的逆置。 要求：自己设计函数的参数，返回值。

--- zr1107/zr1107/test.c
+++ zr1107/zr1107/test.c
@@ -1,29 +1,46 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-int Isleap(int i){
-	if (i % 100 != 0){
-		if (i % 4 == 0){
-			return 1;
-		}
-		return 0;
+int init(int arr[],int len){
+	int i = 0;
+	int num = 0;
+	printf("init numbers,please input five numbers:\n");
+	for (i = 0; i < len; ++i){
+		scanf("%d", &num);
+		arr[i] = num;
 	}
-	if (i % 400 == 0){
-		return 1;
+}
+void empty(int arr[],int len){
+	int i = 0;
+	printf("empty:\n");
+	for (i = 0; i < len; ++i){
+		arr[i] = 0;
 	}
-	return 0;
+}
+void reverse(int arr[],int len){
+	int i = 0;
+	int arr2[5];
+	printf("reverse:\n");
+	for (i = 0; i < len; ++i){
+		arr2[i] = arr[i];
+	}
+	for (i = 0; i < len; ++i){
+		arr[i] = arr2[len - 1 - i];
+	}
 }
 int main(){
-	int ret = 0;
-	int year = 0;
-	printf("input a number:");
-	scanf("%d",&year);
-	ret = Isleap(year);
-	if (ret == 1){
-		printf("it is a leap year");
+	int arr[5] = {0};
+	int len = sizeof(arr) / sizeof(arr[0]);
+	init(arr,len);
+	reverse(arr, len);
+	for (int i = 0; i < len; ++i){
+		printf("%d\t", arr[i]);
 	}
-	else{
-		printf("it is not a leap year");
+	printf("\n");
+	empty(arr,len);
+	for (int i = 0; i < len; ++i){
+		printf("%d\t", arr[i]);
 	}
+	printf("\n");
 	system("pause");
 	return 0;
 }
