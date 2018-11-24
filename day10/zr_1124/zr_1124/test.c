06f56c296d377a522c27e4cea13e35b4012d5200
zr1997 <1007469685@qq.com>
2018-11-24 23:58:28 +0800

    输出一个整数的每一位

---
+++ day10/zr_1124/zr_1124/test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int Printf_num(int num){
+	if (num != 0 ){
+		printf("%d", num % 10);
+		Printf_num(num / 10);
+	}
+	printf("%d", num);
+}
+int main(){
+	int num = 1234;
+	Printf_num(num);
+	system("pause");
+	return 0;
+}
